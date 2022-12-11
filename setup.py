3599672d8f26ef02be6764080391a407ad45646b
Sayan Roy <rsayan553@gmail.com>
2022-12-11 14:57:04 +0530

    the version of the package is updated

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if os.path.isfile(requirement_path):
 
 setuptools.setup(
     name = "imathlogic",
-    version = "0.0.3",
+    version = "0.0.4",
     author = "Sayan Roy",
     author_email = "rsayan553@gmail.com",
     url = "https://github.com/Sayan-Roy-729/iMathLogic",
